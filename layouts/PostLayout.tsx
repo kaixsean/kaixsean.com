887e99d318982134e6650e4c5542ab358da5a498
kaixsean <kxakai@gmail.com>
2024-03-04 13:32:01 +0000

    feat: modify comment feature

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -133,6 +133,11 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               )}
             </div>
           </footer>
+          {siteMetadata.comments && (
+            <div className="divide-y divide-gray-100 py-6 transition-colors dark:divide-gray-800" id="comment">
+              <Comments slug={slug} />
+            </div>
+          )}
         </div>
       </article>
     </SectionContainer>
