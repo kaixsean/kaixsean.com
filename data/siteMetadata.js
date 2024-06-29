ff9da3fda1bb0acb24f37a7b2facd361a31029cf
kaixsean <kxakai@gmail.com>
2024-06-29 02:15:23 +0000

    fix: site's lang setting

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com/in/kaixiangchen',
   threads: 'https://www.threads.net/@kaixsean',
   instagram: 'https://www.instagram.com/kaixsean',
-  locale: 'en-US',
+  locale: 'zh-TW',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
