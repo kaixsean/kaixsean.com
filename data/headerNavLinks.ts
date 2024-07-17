eeca27bd4bc869b4a0932c6ccdabfa8054041ba6
kaixsean <kxakai@gmail.com>
2024-07-17 13:11:28 +0000

    feat: add travel link

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/posts', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
+  { href: '/tags/travel', title: 'Travels' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
