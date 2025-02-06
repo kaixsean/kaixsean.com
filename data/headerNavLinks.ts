9f0c3c66532ede9b7e5a564cbe6a110205e20dc5
kaixsean <kxakai@gmail.com>
2025-02-06 13:52:08 +0000

    feat: add new links to header nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/posts', title: 'Posts' },
   { href: '/tags', title: 'Tags' },
   { href: '/tags/travel', title: 'Travels' },
+  { href: 'https://buymeacoffee.com/kaixdev', title: 'Support' },
   // { href: '/projects', title: 'Projects' },
   { href: 'https://portfolio.kaixsean.com', title: 'About' },
 ]
