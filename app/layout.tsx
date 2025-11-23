6d40844f425f30762319fca2866d149043315772
kaixsean <kxakai@gmail.com>
2025-11-23 04:57:54 +0000

    feat: add google adsense script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,6 +95,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2592283894152153"
+          crossOrigin="anonymous"
+        ></script>
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
